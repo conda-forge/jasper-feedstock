47ce1fb8cad5b04de77a3ee2fe30fd366785c180
Mark Harfouche <mark.harfouche@gmail.com>
2020-10-23 16:45:02 -0700

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,14 @@ declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${HOST} =~ .*linux.* ]]; then
     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
+# Todo, disable OpenGL only for aarch64 and ppc64le
 
 mkdir build_shared && cd $_
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=False \
+    -DJAS_ENABLE_OPENGL=False \
     ${CMAKE_PLATFORM_FLAGS[@]} ..
 make
 make install
